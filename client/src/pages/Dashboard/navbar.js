d5f5bdeec545656c41425d6af104f41c60f40a65
Ricardo <ricardo_slb_brites@live.ca>
2020-04-08 14:14:48 -0400

    First commit

--- client/src/pages/Dashboard/navbar.js
+++ client/src/pages/Dashboard/navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
-import { Button } from "@material-ui/core";
 import Toolbar from "@material-ui/core/Toolbar";
 import useStyles from "./styles/navbar-styles";
 import logo from "../../assets/logo.png";
@@ -8,25 +7,18 @@ import logo from "../../assets/logo.png";
 const profilePicImage =
   "https://i2-prod.mirror.co.uk/incoming/article10883656.ece/ALTERNATES/s615b/PROD-Lost-In-Space-Anniversary-party.jpg";
 
-function Navbar(props) {
+export default function Navbar() {
   const classes = useStyles();
-
-  const handleLogout = () => {
-    props.history.push("/");
-    props.handleLogout();
-  };
-
   return (
     <AppBar position="sticky">
       <Toolbar className={classes.nav}>
         <div className={classes.alignLeft}>
-          <img src={logo} alt="" />
+          <img src={logo} alt="" srcset="" />
         </div>
         <div className={classes.alignRight}>
           <p>Shopping Lists</p>
           <p>Friends</p>
           <p>Notifications</p>
-          <Button onClick={handleLogout}>Logout</Button>
           <div className={classes.circular}>
             <img src={profilePicImage} alt="profile-pic" />
           </div>
@@ -36,5 +28,3 @@ function Navbar(props) {
     </AppBar>
   );
 }
-
-export default Navbar;
