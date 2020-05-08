62a6ab523848023bfa22d4b8275262370e2dd61f
Sagnik Roy <sagnikr95@gmail.com>
2020-05-08 01:14:34 -0400

    Implemented stagnant friends tab in friends page

--- client/src/pages/Followers/styles/FollowerStyles.js
+++ client/src/pages/Followers/styles/FollowerStyles.js
@@ -4,22 +4,25 @@ const followerStyles = makeStyles({
   cardBox: {
     backgroundColor: "white",
     minHeight: "50px",
+    maxHeight: "68vh",
     minWidth: "100%",
     paddingTop: 0,
     paddingBottom: 0,
     paddingRight: 0,
-    paddingLeft: 0
+    paddingLeft: 0,
+
+    overflowY: "scroll"
   },
   followersContainer: {
     marginTop: 30,
-    width: "60%",
+    width: "60vw",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center"
   },
   tabBox: {
-    marginTop: 30,
+    marginTop: 25,
     minWidth: "100%",
     paddingTop: 0,
     paddingBottom: 0,
